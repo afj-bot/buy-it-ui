832c5a166dde2fba8fd1470faa0f2112b1470b29
TomashGombosh <blackjneco@gmail.com>
2022-08-02 08:48:12 +0300

    [INFRA] Change from Typescript to the Javascript

---
+++  .eslintrc.js
@@ -0,0 +1,45 @@
+module.exports = {
+    env: {
+        browser: true,
+        es6: true,
+    },
+    extends: [
+        "eslint:recommended",
+        "plugin:react/recommended",
+        "plugin:react-hooks/recommended",
+    ],
+    parserOptions: {
+        ecmaFeatures: {
+            jsx: true,
+        },
+        ecmaVersion: 6,
+        sourceType: "module",
+    },
+    globals: {
+        eslint: true,
+        expect: true,
+        it: true,
+        xit: true,
+        describe: true,
+        xdescribe: true,
+        jest: true,
+        process: true,
+        global: true,
+        afterEach: true,
+        beforeEach: true,
+        beforeAll: true,
+        module: true,
+        __dirname: true,
+    },
+    plugins: ["react", "prettier"],
+    rules: {
+        "prettier/prettier": "error",
+        "no-magic-numbers": ["error", { ignore: [-1, 0, 1, 2, 3, 4, 5] }],
+    },
+    parser: "babel-eslint",
+    settings: {
+        react: {
+            version: "detect",
+        },
+    },
+};
---
+++ src/App.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+import "./App.css";
+
+function App() {
+  return (
+    <header className="header">
+      Welcome
+    </header>
+  );
+}
+
+export default App;
