3bdbc283c0d1fdb492a69a8a113c9e1abca23b58
TomashGombosh <blackjneco@gmail.com>
2022-08-04 17:38:14 +0300

    [DevOPS] Update the build image

--- tools/install_dependencies.sh
+++ tools/install_dependencies.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 echo "Install all dependencies"
-apt-get update
-apt-get install -y unzip curl zip
+sudo apt-get update
+sudo apt-get install -y unzip curl zip
+
+node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
+sudo apt-get update
 
