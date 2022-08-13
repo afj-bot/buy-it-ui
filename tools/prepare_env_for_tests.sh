6ae360c841ccccdb500b0b8d6e7040d95584d51b
TomashGombosh <blackjneco@gmail.com>
2022-08-13 21:45:10 +0300

    [DevOPS] Change the java version

--- tools/prepare_env_for_tests.sh
+++ tools/prepare_env_for_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Intall Java tar"
-sudo apt-get install -y tar curl openjdk-17-jdk-headless
+sudo apt-get install -y tar curl openjdk-17-jdk
 
 echo "Install node"
 node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
