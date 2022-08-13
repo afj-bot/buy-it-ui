74e0a7165de06cfb32020e1f006b99e803ecbe0e
TomashGombosh <blackjneco@gmail.com>
2022-08-13 22:04:21 +0300

    [DevOPS] Change upload and add allure-command line

--- tools/prepare_env_for_tests.sh
+++ tools/prepare_env_for_tests.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 echo "Intall Java tar"
-sudo apt-get install -y tar curl openjdk-17-jre-headless
+sudo apt-get install -y unzip curl tar openjdk-17-jre-headless
 
 echo "Install node"
 node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
@@ -11,6 +11,3 @@ java --version
 
 echo "Intall project dependecies"
 npm install 
-
-echo "Clean up"
-sudo rm -r allure-2.18.1.tgz
