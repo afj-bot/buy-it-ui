3165fcd4b696693cd5b60c037c0d8587dc05d591
TomashGombosh <blackjneco@gmail.com>
2022-08-13 21:58:52 +0300

    [DevOPS] Change upload and add allure-command line

--- tools/prepare_env_for_tests.sh
+++ tools/prepare_env_for_tests.sh
@@ -6,6 +6,9 @@ echo "Install node"
 node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
 sudo apt-get update
 
+node --version
+java --version
+
 echo "Intall project dependecies"
 npm install 
 
