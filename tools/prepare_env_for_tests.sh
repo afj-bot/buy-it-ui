c608dae5d39b3aa6d244d30bb228bb4f1865a9bb
TomashGombosh <blackjneco@gmail.com>
2022-08-13 21:34:39 +0300

    [DevOPS] Update the circle ci workflow

---
+++ tools/prepare_env_for_tests.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+sh ./install_dependencies.sh    
+echo "Intall Java, cURL and tar"
+sudo apt-get install -y unzip curl tar openjdk-17-jre-headless
+sudo apt-get update
+echo "Intall allure"
+curl -o allure-2.18.1.tgz -OLs https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.18.1/allure-commandline-2.18.1.tgz
+sudo tar -zxvf allure-2.18.1.tgz -C /opt/
+sudo ln -s /opt/allure-2.18.1/bin/allure /usr/bin/allure
+echo "Intall project dependecies"
+npm install 
