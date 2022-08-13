e301216e0c8557f2d5fe4478e02fb4cc1e707cd5
TomashGombosh <blackjneco@gmail.com>
2022-08-13 22:08:36 +0300

    [DevOPS] Java installiation

--- tools/prepare_env_for_tests.sh
+++ tools/prepare_env_for_tests.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 echo "Intall Java tar"
-sudo apt-get install -y unzip curl tar openjdk-17-jre-headless
+sudo apt-get install -y curl tar 
+sudo apt-get install -y openjdk-17-jre-headless
 
 echo "Install node"
 node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
