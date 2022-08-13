261b0993a5e665ef42f7f860c47c9fcc06ff38f6
TomashGombosh <blackjneco@gmail.com>
2022-08-13 21:54:59 +0300

    [DevOPS] Change upload and add allure-command line

--- tools/prepare_env_for_tests.sh
+++ tools/prepare_env_for_tests.sh
@@ -6,10 +6,6 @@ echo "Install node"
 node -v | grep v15 || curl -sL https://deb.nodesource.com/setup_16.x | sudo -E bash - && yes|sudo apt-get install nodejs && yes|sudo apt-get install libxss1
 sudo apt-get update
 
-echo "Intall allure"
-curl -o allure-2.18.1.tgz -OLs https://repo.maven.apache.org/maven2/io/qameta/allure/allure-commandline/2.18.1/allure-commandline-2.18.1.tgz
-sudo tar -zxvf allure-2.18.1.tgz -C /opt/
-sudo ln -s /opt/allure-2.18.1/bin/allure /usr/bin/allure
 echo "Intall project dependecies"
 npm install 
 
