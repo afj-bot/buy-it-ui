8ce201e9bb77d01ff296a4cd0aaa68e38dff5774
TomashGombosh <blackjneco@gmail.com>
2022-08-14 15:35:23 +0300

    [LINT] fix

--- src/components/auth/AuthenticateClosedRoute.jsx
+++ src/components/auth/AuthenticateClosedRoute.jsx
@@ -1,15 +1,20 @@
-import React from "react";
-import { Navigate } from "react-router-dom";
+import React from 'react'
+import PropTypes from 'prop-types'
+import { Navigate } from 'react-router-dom'
 
-import { PUBLIC_ROUTES, AUTH_TOKEN_ATTRIBUTE, ANONYMOUS_ATTRIBUTE } from "../../constants";
+import { PUBLIC_ROUTES, AUTH_TOKEN_ATTRIBUTE, ANONYMOUS_ATTRIBUTE } from '../../constants'
 
 const AuthenticateClosedRoute = ({ children }) => {
-    if (localStorage.getItem(AUTH_TOKEN_ATTRIBUTE) !== null && localStorage.getItem(ANONYMOUS_ATTRIBUTE) !== "true") {
-        return (
+  if (localStorage.getItem(AUTH_TOKEN_ATTRIBUTE) !== null && localStorage.getItem(ANONYMOUS_ATTRIBUTE) !== 'true') {
+    return (
             <Navigate to={PUBLIC_ROUTES.DASHBOARD} replace={true} />
-        );
-    }
-    return children;
-};
+    )
+  }
+  return children
+}
 
-export default AuthenticateClosedRoute;
+AuthenticateClosedRoute.propTypes = {
+  children: PropTypes.node
+}
+
+export default AuthenticateClosedRoute
--- src/index.js
+++ src/index.js
@@ -2,18 +2,17 @@ import React, { Suspense } from 'react'
 import ReactDOM from 'react-dom/client'
 import './index.css'
 
-import Loading from "./components/loader/Loading";
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
-import AuthenticatedRoute from './components/auth/AuthenticatedRoute';
-import AuthenticateClosedRoute from './components/auth/AuthenticateClosedRoute';
-import { AUTH_ROUTES, PUBLIC_ROUTES } from './constants';
-import LocalizeProvider from './service/providers/LocalizeProvider';
+import Loading from './components/loader/Loading'
+import reportWebVitals from './reportWebVitals'
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom'
+import AuthenticatedRoute from './components/auth/AuthenticatedRoute'
+import AuthenticateClosedRoute from './components/auth/AuthenticateClosedRoute'
+import { AUTH_ROUTES, PUBLIC_ROUTES } from './constants'
+import LocalizeProvider from './service/providers/LocalizeProvider'
 
-
-const App = React.lazy(() => import("./App"));
-const Login = React.lazy(() => import("./pages/Login"));
-const ForgotPassoword = React.lazy(() => import("./pages/forgotPassword/ForgotPassoword"));
+const App = React.lazy(() => import('./App'))
+const Login = React.lazy(() => import('./pages/Login'))
+const ForgotPassoword = React.lazy(() => import('./pages/forgotPassword/ForgotPassoword'))
 
 class Index extends React.Component {
   renderLogin = () => (
@@ -22,7 +21,7 @@ class Index extends React.Component {
         <Login />
       </App>
     </AuthenticateClosedRoute>
-  );
+  )
 
   renderMyProfile = () => (
     <AuthenticatedRoute>
