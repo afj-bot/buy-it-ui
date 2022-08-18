c5d6e5ba2e6150aa66bac8d2de936792b2edd901
TomashGombosh <blackjneco@gmail.com>
2022-08-18 22:27:45 +0300

    [LINT] Fix linter rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -33,9 +33,8 @@ module.exports = {
     "react"
   ],
   rules: {
-    "no-magic-numbers": ["error", { ignore: [-1, 0, 1, 2, 3, 4, 5] }],
-    quotes: ["error", "double"],
-    "semi": ["always"]
+    "no-magic-numbers": ["error", { ignore: [0, 1, 2, 3, 4, 5] }],
+    quotes: ["error", "double"]
   },
   settings: {
     react: {
