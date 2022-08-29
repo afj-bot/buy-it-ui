918efa26e48cb881982f25a4492fc4af0af0e061
TomashGombosh <blackjneco@gmail.com>
2022-08-29 22:03:44 +0300

    [INFRA] CORS policy

--- src/service/api/axios.js
+++ src/service/api/axios.js
@@ -10,10 +10,12 @@ const instance = axios.create({
   baseURL: API_BASE_URL,
   headers: {
     "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "*",
     "Accept-Language": localStorage.getItem(LANGUAGE_ATTRIBUTE) !== null ? localStorage.getItem(LANGUAGE_ATTRIBUTE) : "en",
   },
   timeout: 20000,
   withCredentials: true,
+  credentials: "same-origin",
   validateStatus: function (status) {
     return status >= OK && status < SERVER_ERROR;
   },
