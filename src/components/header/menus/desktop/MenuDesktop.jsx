1ae33a0060157e009c2aae292604b80df63f30f1
Tomash Gombosh <blackjneco@gmail.com>
2022-11-29 22:52:46 +0200

    [TEST] Add data test labels (#37)

--- src/components/header/menus/desktop/MenuDesktop.jsx
+++ src/components/header/menus/desktop/MenuDesktop.jsx
@@ -12,29 +12,57 @@ const MenuDesktop = ({ items, login, cartItems, isShowLogin }) => {
   const { getKeyValue } = useContext(LocalizeContext);
 
   return (
-        <>
-        {items.map((it) => (
-            <Grid item key={it.value} className="button-container">
-                <Button component={Link} to={it.link} className="button">{getKeyValue(it.value)}</Button>
-            </Grid>
-        ))}
-        <Grid item id="card-and-search">
-             <Grid container direction="row" alignContent="center" justifyContent="center" alignItems="center">
-                {isShowLogin && <Grid key={login.value} item className="icon">
-                    <IconButton component={Link} to={login.link} className="icon-desktop">
-                        {createElement(login.icon, { className: "icon-desktop" })}
-                    </IconButton>
-                </Grid>}
-                {cartItems.map((it) => (
-                <Grid key={it.value} item className="icon">
-                    <IconButton component={Link} to={it.link} className="icon-desktop">
-                        {createElement(it.icon, { className: "icon-desktop" })}
-                    </IconButton>
-                </Grid>
-                ))}
-            </Grid>
+    <>
+      {items.map((it) => (
+        <Grid item key={it.value} className="button-container">
+          <Button
+            component={Link}
+            to={it.link}
+            className="button"
+            data-testid={it.value}
+          >
+            {getKeyValue(it.value)}
+          </Button>
         </Grid>
-        </>
+      ))}
+      <Grid item id="card-and-search">
+        <Grid
+          container
+          direction="row"
+          alignContent="center"
+          justifyContent="center"
+          alignItems="center"
+        >
+          {isShowLogin && (
+            <Grid
+              key={login.value}
+              data-testid={login.value}
+              item
+              className="icon"
+            >
+              <IconButton
+                component={Link}
+                to={login.link}
+                className="icon-desktop"
+              >
+                {createElement(login.icon, { className: "icon-desktop" })}
+              </IconButton>
+            </Grid>
+          )}
+          {cartItems.map((it) => (
+            <Grid key={it.value} data-testid={it.value} item className="icon">
+              <IconButton
+                component={Link}
+                to={it.link}
+                className="icon-desktop"
+              >
+                {createElement(it.icon, { className: "icon-desktop" })}
+              </IconButton>
+            </Grid>
+          ))}
+        </Grid>
+      </Grid>
+    </>
   );
 };
 
